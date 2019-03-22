cd722e0dd45e90e3300a9ee3511cb038670cdfee
rmy17 mail = remyvi17@gmail.com <remyvi17@gmail.com>
2019-03-22 16:10:45 +0100

    profils DEV et PROD

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,8 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        String asciiArt = FigletFont.convertOneLine("\"\"fireworks\"\"");
-        System.out.println(asciiArt);
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);
+    	// affichage de l'environnement
+    	String environnement = ResourceBundle.getBundle("application").getString(
+    	"environnement");
+    	System.out.println("Environnement : " + environnement);
     }
 }
