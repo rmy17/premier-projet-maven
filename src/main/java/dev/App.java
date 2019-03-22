c84e23c110adc4c2017bbf5671bfa9d69936b32a
rmy17 mail = remyvi17@gmail.com <remyvi17@gmail.com>
2019-03-22 11:55:10 +0100

    ajout package jfiglet

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String asciiArt = FigletFont.convertOneLine("\"\"fireworks\"\"");
+        System.out.println(asciiArt);
     }
 }
